66258b85f3836a4e15b9670aaa242e849c6b71eb
kaosborn <kaosborn@users.noreply.github.com>
2017-05-21 18:22:06 -0700

    first

---
+++ build.cmd
@@ -0,0 +1,17 @@
+@rem 1. Scaffold a new blog from project root:
+@rem      wyam.exe new --recipe Blog
+@rem 2. Create repository at github and clone:
+@rem      git clone https://github.com/kaosborn/kaosborn.github.io
+@rem 3. Execute this script:
+@rem      build
+@rem 4. If all is good, then publish:
+@rem      git commit -m rebuild
+@rem      git push -u origin master
+pushd kaosborn.github.io
+git rm -rfq .
+popd
+wyam.exe --recipe Blog --theme CleanBlog --output kaosborn.github.io --noclean
+pushd kaosborn.github.io
+git add *
+git add .nojekyll
+popd
---
+++ input/about.md
@@ -0,0 +1,3 @@
+Title: About
+---
+Systems programming in .Net
