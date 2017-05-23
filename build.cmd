889c96138d1e725d09bf9d0db14d201b466a4243
kaosborn <kaosborn@users.noreply.github.com>
2017-05-22 18:06:39 -0700

    change comments

--- build.cmd
+++ build.cmd
@@ -1,11 +1,11 @@
 @rem 1. Scaffold a new blog from project root:
 @rem      wyam.exe new --recipe Blog
 @rem 2. Create repository at github and clone:
-@rem      git clone https://github.com/kaosborn/kaosborn.github.io
+@rem      git clone https://github.com/kaosborn/kaosborn.github.io.git
 @rem 3. Execute this script:
 @rem      build
 @rem 4. If all is good, then publish:
-@rem      git commit -m rebuild
+@rem      git commit -m built
 @rem      git push -u origin master
 pushd kaosborn.github.io
 git rm -rfq .
